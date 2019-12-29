440af1e5fd757026779c461e9b05d7ae9a2ad124
michael-franke <mchfranke@gmail.com>
2019-12-29 22:11:39 +0100

    strong emphasis on direction words in trial instructions

--- 04_trials.js
+++ 04_trials.js
@@ -4,47 +4,47 @@ const trial_info_ = {
     textbox_input: [
         {
             question: "New blankets for the kid's beds. Costs: <b> $4,988</b>" ,
-            QUD: "In the textbox below, revise the expense upwards so that you won't loose money for next year's budget.",
+            QUD: "In the textbox below, revise the expense <b>upwards</b> so that you won't loose money for next year's budget.",
             min_chars: 0
         },
         {
             question: "New medical equipment. Costs: <b> $82,796</b>",
-            QUD: "In the textbox below, revise the expense upwards so that you won't loose money for next year's budget.",
+            QUD: "In the textbox below, revise the expense <b>upwards</b> so that you won't loose money for next year's budget.",
             min_chars: 0
         },
         {
             question: "Food supplier expenses. Costs: <b>$21,337</b>",
-            QUD: "In the textbox below, revise the expense upwards so that you won't loose money for next year's budget.",
+            QUD: "In the textbox below, revise the expense <b>upwards</b> so that you won't loose money for next year's budget.",
             min_chars: 0
         },
         {
             question: "Salary expenses. Costs: <b>$551,140</b>",
-            QUD: "In the textbox below, revise the expense upwards so that you won't loose money for next year's budget.",
+            QUD: "In the textbox below, revise the expense <b>upwards</b> so that you won't loose money for next year's budget.",
             min_chars: 0
         },
         {
             question: "Drug expenses. Costs:<b> $18,265</b>",
-            QUD: "In the textbox below, revise the expense upwards so that you won't loose money for next year's budget.",
+            QUD: "In the textbox below, revise the expense <b>upwards</b> so that you won't loose money for next year's budget.",
             min_chars: 0
         },
         {
             question: "Christmas party expenses. Costs: <b>$3,195</b>",
-            QUD: "In the textbox below, revise the expense upwards so that you won't loose money for next year's budget.",
+            QUD: "In the textbox below, revise the expense <b>upwards</b> so that you won't loose money for next year's budget.",
             min_chars: 0
         },
         {
             question: "Cancer research. Costs:<b> $76,420</b>",
-            QUD: "In the textbox below, revise the expense upwards so that you won't loose money for next year's budget.",
+            QUD: "In the textbox below, revise the expense <b>upwards</b> so that you won't loose money for next year's budget.",
             min_chars: 0
         },
         {
             question: "Clown hired for the entertainment of the kids. Costs: <b>$9,036</b>",
-            QUD: "In the textbox below, revise the expense upwards so that you won't loose money for next year's budget.",
+            QUD: "In the textbox below, revise the expense <b>upwards</b> so that you won't loose money for next year's budget.",
             min_chars: 0
         },
         {
             question: "Toys for the children's entertainment room. Costs:<b> $6,450</b>",
-            QUD: "In the textbox below, revise the expense upwards so that you won't loose money for next year's budget.",
+            QUD: "In the textbox below, revise the expense <b>upwards</b> so that you won't loose money for next year's budget.",
             min_chars: 0
         }
       ]
@@ -54,47 +54,47 @@ const trial_info_B = {
         textbox_input: [
             {
                 question: "New blankets for the kid's beds. Costs: <b> $4,988</b>" ,
-                QUD: "In the textbox below, revise the expense downwards so that you won't loose money for next year's budget.",
+                QUD: "In the textbox below, revise the expense <b>downwards</b> so that you won't loose money for next year's budget.",
                 min_chars: 0
             },
             {
                 question: "New medical equipment. Costs: <b> $82,796</b>",
-                QUD: "In the textbox below, revise the expense downwards so that you won't loose money for next year's budget.",
+                QUD: "In the textbox below, revise the expense <b>downwards</b> so that you won't loose money for next year's budget.",
                 min_chars: 0
             },
             {
                 question: "Food supplier expenses. Costs: <b>$21,337</b>",
-                QUD: "In the textbox below, revise the expense downwards so that you won't loose money for next year's budget.",
+                QUD: "In the textbox below, revise the expense <b>downwards</b> so that you won't loose money for next year's budget.",
                 min_chars: 0
             },
             {
                 question: "Salary expenses. Costs: <b>$551,140</b>",
-                QUD: "In the textbox below, revise the expense downwards so that you won't loose money for next year's budget.",
+                QUD: "In the textbox below, revise the expense <b>downwards</b> so that you won't loose money for next year's budget.",
                 min_chars: 0
             },
             {
                 question: "Drug expenses. Costs:<b> $18,265</b>",
-                QUD: "In the textbox below, revise the expense downwards so that you won't loose money for next year's budget.",
+                QUD: "In the textbox below, revise the expense <b>downwards</b> so that you won't loose money for next year's budget.",
                 min_chars: 0
             },
             {
                 question: "Christmas party expenses. Costs: <b>$3,195</b>",
-                QUD: "In the textbox below, revise the expense downwards so that you won't loose money for next year's budget.",
+                QUD: "In the textbox below, revise the expense <b>downwards</b> so that you won't loose money for next year's budget.",
                 min_chars: 0
             },
             {
                 question: "Cancer research. Costs:<b> $76,420</b>",
-                QUD: "In the textbox below, revise the expense downwards so that you won't loose money for next year's budget.",
+                QUD: "In the textbox below, revise the expense <b>downwards</b> so that you won't loose money for next year's budget.",
                 min_chars: 0
             },
             {
                 question: "Clown hired for the entertainment of the kids. Costs: <b>$9,036</b>",
-                QUD: "In the textbox below, revise the expense downwards so that you won't loose money for next year's budget.",
+                QUD: "In the textbox below, revise the expense <b>downwards</b> so that you won't loose money for next year's budget.",
                 min_chars: 0
             },
             {
                 question: "Toys for the children's entertainment room. Costs:<b> $6,450</b>",
-                QUD: "In the textbox below, revise the expense downwards so that you won't loose money for next year's budget.",
+                QUD: "In the textbox below, revise the expense <b>downwards</b> so that you won't loose money for next year's budget.",
                 min_chars: 0
             }
           ]
