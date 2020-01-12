5303c7329cce6563ce10ff207a22f79f21b93653
Nina Mainusch <55274916+Nina-Mainusch@users.noreply.github.com>
2020-01-12 23:36:33 +0100

    activated database

--- 06_main.js
+++ 06_main.js
@@ -35,13 +35,13 @@ $("document").ready(function() {
 
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "INSERT_A_NUMBER",
+            experimentID: "88",
             serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "debug",
-            contact_email: "YOUREMAIL@wherelifeisgreat.you",
+            deployMethod: "Prolific",
+            contact_email: "nmainusch@uos.de",
             prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
         // Here, you can specify how the progress bar should look like
